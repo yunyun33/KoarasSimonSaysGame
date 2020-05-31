934c2a015310cc32e296f39254102f02b67c586c
yuikonno <>
2020-05-31 16:16:46 +0900

    make the tableView(saved nameAndScore, but can not display to cell)

--- KoarasSimonSaysGame/RankingDateViewController.swift
+++ KoarasSimonSaysGame/RankingDateViewController.swift
@@ -11,28 +11,38 @@ import UIKit
 class RankingDateViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-    private var items: [String] = ["1", "2", "3"]
+    var nameAndScore: [[String]] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.contentInset = UIEdgeInsets(top: 30, left: 0, bottom: 0, right: 0)
+        
     }
     
-
-    
-
+    override func viewWillAppear(_ animated: Bool) {
+               
+        nameAndScore.removeAll()
+               
+        if let savedname = UserDefaults.standard.array(forKey: "nameAndScore") as? [[String]] {
+            nameAndScore = savedname
+            print(nameAndScore)
+        }
+        tableView.reloadData()
+    }
 }
 
 extension RankingDateViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return nameAndScore.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")
-        cell?.textLabel?.text = items[indexPath.row]
-        
+        let ranking = nameAndScore[indexPath.row]
+        cell?.textLabel?.text = "\(indexPath.row + 1) \(ranking) "
+      
         return cell ?? UITableViewCell()
     }
 }
