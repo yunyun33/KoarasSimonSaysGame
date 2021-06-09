9c0982315481152995bfb0a3342cb53d37e69a4f
yuikonno <>
2021-06-09 21:35:14 +0900

    リンクのURLを定義する。

--- KoarasSimonSaysGame/Pages/Common/CommonValue.swift
+++ KoarasSimonSaysGame/Pages/Common/CommonValue.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+//画面間でデータを共有、参照するための構造体を定義
+
+struct CommonValue {
+    struct URL {
+        // 開発者の連絡先(HP)
+        static let illustratorContactURL = "https://yunyun33.com/"
+        
+        //プライバシーポリシー
+        static let privacyPolicyURL = "https://yunyun33.com/koalasgame-privacy_policy/"
+        
+        //コアラさんのニート診断
+        static let neetCheckAppStorePage = "https://apps.apple.com/jp/app/%E3%82%B3%E3%82%A2%E3%83%A9%E3%81%95%E3%82%93%E3%81%AE%E3%83%8B%E3%83%BC%E3%83%88%E3%83%81%E3%82%A7%E3%83%83%E3%82%AF/id1456451456?mt=8"
+        
+        //コアラさんのおうちカフェ
+        static let ouchiCafeAppAppStorePage =  "https://apps.apple.com/jp/app/%E3%82%B3%E3%82%A2%E3%83%A9%E3%81%95%E3%82%93%E3%81%AE%E3%81%8A%E3%81%86%E3%81%A1%E3%82%AB%E3%83%95%E3%82%A7/id1493333294?mt=8"
+    }
+}
