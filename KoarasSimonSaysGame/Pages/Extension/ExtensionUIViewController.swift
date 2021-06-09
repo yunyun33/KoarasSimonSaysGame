82f1399048f03fb488fe501618c4f2cabad70ef3
yuikonno <>
2021-06-09 21:30:43 +0900

    リファクタリングのためフォルダやファイルを追加する。

---
+++ KoarasSimonSaysGame/Pages/Extension/ExtensionUIViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ExtensionUIViewController.swift
+//  KoarasSimonSaysGame
+//
+//  Created by yuikonno on 2021/06/09.
+//  Copyright © 2021 yuikonno. All rights reserved.
+//
+
+import Foundation
