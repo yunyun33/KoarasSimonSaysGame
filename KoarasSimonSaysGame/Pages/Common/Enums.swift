7166a79100874d5bdc261f50248cdabc14059496
yuikonno <>
2021-06-11 00:55:23 +0900

    enumで旗上げの方向のStringを定義する。

--- KoarasSimonSaysGame/Pages/Common/Enums.swift
+++ KoarasSimonSaysGame/Pages/Common/Enums.swift
@@ -9,9 +9,22 @@
 import Foundation
 
 //旗上げの方向を定義
-enum Direction {
+enum Direction: CaseIterable {
     case UP
     case DOWN
     case RIGHT
     case LEFT
+    
+    func getInstructionText() -> String {
+        switch self {
+        case .UP:
+            return "上あげて！"
+        case .DOWN:
+            return "下さげて！"
+        case .RIGHT:
+            return "右にして！"
+        case .LEFT:
+            return "左にして！"
+        }
+    }
 }
