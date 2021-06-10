ca6a6c6ec5459529d16742c709d6df91a57a031f
yuikonno <>
2021-06-10 22:44:08 +0900

    enumで旗上げの方向を定義する。

---
+++ KoarasSimonSaysGame/Pages/Common/Enums.swift
@@ -0,0 +1,17 @@
+//
+//  Enums.swift
+//  KoarasSimonSaysGame
+//
+//  Created by yuikonno on 2021/06/10.
+//  Copyright © 2021 yuikonno. All rights reserved.
+//
+
+import Foundation
+
+//旗上げの方向を定義
+enum Direction {
+    case UP
+    case DOWN
+    case RIGHT
+    case LEFT
+}
