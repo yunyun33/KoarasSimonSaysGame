86d129e3f2e7b9391c9cea8bf18315696955b29a
yuikonno <>
2021-06-16 15:14:52 +0900

    RankingModelに登録する処理を書く。

--- KoarasSimonSaysGame/Model/RankingModel.swift
+++ KoarasSimonSaysGame/Model/RankingModel.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+import Firebase
+
+protocol RankingModelProtocol {
+    func saveToUserDefaults(name: String, score: Int)
+    func seveToFirestore(name: String, score: Int)
+}
+
+class RankingModel: RankingModelProtocol {
+    
+    var defaultstore:Firestore!
+    
+    func saveToUserDefaults(name: String, score: Int) {
+        // 保存するメモ情報を配列にする 0: 名前, 1: スコア
+        let rankingToSave: [String] = [name, "\(score)"]
+        
+        // すでに保存されているメモがあれば追加して保存
+        if var memo: [[String]] = UserDefaults.standard.array(forKey: "nameAndScore") as? [[String]] {
+            
+            memo.append(rankingToSave)
+            
+            //並び替え。同立の場合、最初にgameした人が上に表示される
+            let memoSorted = memo.sorted(by: {Double($0[1])! > Double($1[1])!})
+            
+            UserDefaults.standard.set(memoSorted, forKey: "nameAndScore")
+            
+        } else {
+            // 保存しているメモがなければ新規で保存
+            UserDefaults.standard.set([rankingToSave], forKey: "nameAndScore")
+        }
+    }
+    
+    //データをFirestoreに保存
+    func seveToFirestore(name: String, score: Int) {
+        var ref: DocumentReference? = nil
+        ref = Firestore.firestore().collection("users").addDocument(data: [
+            "rankingName": name,
+            "totalScore": score,
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
+    }
+}
